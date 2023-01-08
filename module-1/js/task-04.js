e57653927eec8dd2d38888f06efb7c7cb9353808
valentine-e <valia101112@gmail.com>
2023-01-08 15:03:29 +0200

    1

--- module-1/js/task-04.js
+++ module-1/js/task-04.js
@@ -1,2 +1,19 @@
-const credits = 23580;
-const pricePerDroid = 3000;
+const credits = 10;
+const pricePerDroid = 2;
+
+const howManyUnits = prompt("?");
+
+if (howManyUnits === null) {
+  console.log("canceled");
+} else {
+  const totalPrice = howManyUnits * pricePerDroid;
+  if (totalPrice > credits) {
+    console.log("not ok");
+  } else {
+    console.log(
+      `you bought ${howManyUnits} drones, you still have ${
+        credits - totalPrice
+      } money`
+    );
+  }
+}
--- module-1/js/task-05.js
+++ module-1/js/task-05.js
@@ -7,3 +7,26 @@ const chili = 250;
 const australia = 170;
 const india = 80;
 const jamaica = 120;
+
+switch (userDelivery.toLowerCase()) {
+  case "китай":
+    alert(`доставка стоит ${china} $`);
+    break;
+
+  case "чили":
+    alert(`доставка стоит ${chili} $`);
+    break;
+  case "австралия":
+    alert(`доставка стоит ${australia} $`);
+    break;
+  case "индия":
+    alert(`доставка стоит ${india} $`);
+    break;
+  case "ямайка":
+    alert(`доставка стоит ${jamaica} $`);
+    break;
+
+  default:
+    alert("error country");
+    break;
+}
