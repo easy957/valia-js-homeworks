554a060feda71ee103048d8135d95dd945658438
valentine-e <valia101112@gmail.com>
2023-01-12 15:47:20 +0200

    yo

--- module-4/js/task-02.js
+++ module-4/js/task-02.js
@@ -1,10 +1,12 @@
 const inventory = {
   items: ["Knife", "Gas mask"],
+
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
     this.items.push(itemName);
   },
+
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
@@ -17,13 +19,13 @@ const invokeInventoryAction = function (itemName, action) {
   action(itemName);
 };
 
-invokeInventoryAction("Medkit", inventory.add);
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction("Gas mask", inventory.remove);
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
