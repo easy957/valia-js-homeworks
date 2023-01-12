671f6cfa3525f7e47e87fe9a6daa02b830f91129
easy957 <easy957@gmail.com>
2023-01-12 14:58:24 +0200

    4rd module

--- module-4/js/task-01.js
+++ module-4/js/task-01.js
@@ -1,17 +1,17 @@
 const account = {
-  owner: 'Mango',
+  owner: "Mango",
   balance: 24000,
   discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
+  orders: ["order-1", "order-2", "order-3"],
   changeDiscount(value) {
-    this.discount = value;
+    discount = value;
   },
   showOrders() {
-    return this.orders;
+    return orders;
   },
   addOrder(cost, order) {
-    this.balance -= cost;
-    this.orders.push(order);
+    balance -= cost;
+    orders.push(order);
   },
 };
 
@@ -20,6 +20,6 @@ console.log(account.discount); // 0.15
 
 console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
 
-account.addOrder(5000, 'order-4');
+account.addOrder(5000, "order-4");
 console.log(account.balance); // 19000
 console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
--- module-4/js/task-02.js
+++ module-4/js/task-02.js
@@ -1,5 +1,5 @@
 const inventory = {
-  items: ['Knife', 'Gas mask'],
+  items: ["Knife", "Gas mask"],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
 
@@ -8,23 +8,22 @@ const inventory = {
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
 
-    this.items = this.items.filter(item => item !== itemName);
+    this.items = this.items.filter((item) => item !== itemName);
   },
 };
 
-
-const invokeInventoryAction = function(itemName, action) {
+const invokeInventoryAction = function (itemName, action) {
   console.log(`Invoking action on ${itemName}`);
   action(itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+invokeInventoryAction("Medkit", inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+invokeInventoryAction("Gas mask", inventory.remove);
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
